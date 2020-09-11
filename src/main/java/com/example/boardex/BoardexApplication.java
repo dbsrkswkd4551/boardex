6d1963c3ccf6fab86ad8469b50b4c935f339298f
dbsrkswkd4551 <ysang4512005@naver.com>
2020-09-11 11:02:15 +0900

    Initial commit

---
+++ src/main/java/com/example/boardex/BoardexApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardexApplication.class, args);
+    }
+
+}
