a4376e9d8c6ee597f42b766770922cd6768ca3a3
dbsrkswkd4551 <ysang4512005@naver.com>
2020-09-11 14:49:10 +0900

    @RequestMapping 경로 패턴 매핑

--- src/main/java/com/example/boardex/controller/BoardController.java
+++ src/main/java/com/example/boardex/controller/BoardController.java
@@ -2,11 +2,15 @@ package com.example.boardex.controller;
 
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log
 @Controller
+@RequestMapping("/board")
 public class BoardController {
+    //요청 경로 매핑
+    /*
     @RequestMapping(value = "/board/register")
     public void registerForm() {
         log.info("registerForm");
@@ -22,6 +26,38 @@ public class BoardController {
     public void list() {
         log.info("list");
     }
+     */
+
+    @RequestMapping("/register")
+    public void registerForm() {
+        log.info("registerForm");
+    }
+
+    @RequestMapping("/modify")
+    public void modifyForm() {
+        log.info("modifyForm");
+    }
+
+    @RequestMapping("/list")
+    public void list() {
+        log.info("list");
+    }
+
+
+    @RequestMapping("/read/{boardNo}")
+    public String read(@PathVariable("boardNo")int boardNo){
+        log.info("read boardNo:" + boardNo);
+
+        return "board/read";
+    }
+
+    @RequestMapping("/read2/{no}")
+    public String read2(@PathVariable("no")int boardNo) {
+        log.info("read2 boardNo:" + boardNo);
+
+        return "board/read";
+    }
+
 
 
 }
---
+++ src/main/webapp/WEB-INF/views/board/read.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: miso
+  Date: 2020-09-11
+  Time: 오후 2:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>BOARD</title>
+</head>
+<body>
+    <h3>READ</h3>
+</body>
+</html>
