bf44628b2f33e94bbadbac5b3715a105c5bcec12
dbsrkswkd4551 <ysang4512005@naver.com>
2020-09-11 13:05:47 +0900

    Lombok innotation initial & test

--- src/main/java/com/example/boardex/domain/Board.java
+++ src/main/java/com/example/boardex/domain/Board.java
@@ -1,99 +1,20 @@
 package com.example.boardex.domain;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Required;
 
 import java.sql.Date;
 
-@Getter
-public class Board implements java.io.Serializable{
-    private static final long serialVersionUID = 1L;
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Board{
 
     private int boardNo;
+    @NonNull
     private String title;
     private String content;
     private String writer;
     private Date regDate;
 
-    public Board(){
-        super();
-    }
 
-
-
-
-    public Board(int boardNo, String title){
-        super();
-        this.boardNo = boardNo;
-        this.title = title;
-    }
-
-    public int getBoardNo(){
-        return boardNo;
-    }
-
-    public void setBoardNo(int boardNo){
-        this.boardNo = boardNo;
-    }
-
-    public String getTitle(){
-        return title;
-    }
-
-    public void setTitle(String title){
-        this.title = title;
-    }
-
-    public String getContent(){
-        return content;
-    }
-
-    public void setContent(String content){
-        this.content = content;
-    }
-
-    public String getWriter(){
-        return writer;
-    }
-
-    public void setWriter(String writer){
-        this.writer = writer;
-    }
-
-    public Date getRegDate(){
-        return regDate;
-    }
-
-    public void setRegDate(Date regDate){
-        this.regDate = regDate;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + boardNo;
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj){
-        if(this==obj)
-            return true;
-        if(obj==null)
-            return false;
-        if(getClass() != obj.getClass())
-            return false;
-        Board other = (Board) obj;
-        if(boardNo != other.boardNo)
-            return false;
-        return true;
-
-    }
-
-    @Override
-    public String toString(){
-        return "Board(boardNo=" + boardNo + ", title=" + title + ", content=" + content +
-                ", writer=" + writer + ",regDate=" + regDate + ")";
-    }
 }
